91c3f7caf78d8ad98338c35e18b94ca39c9e7c89
Anirudh Sharma <anirudhology@gmail.com>
2024-04-12 05:09:27 +0530

    LeetCode #39 - Combination Sum
    Add problem solution

---
+++ src/main/java/com/anirudhology/leetcoding/array/CombinationSum.java
@@ -0,0 +1,41 @@
+package com.anirudhology.leetcoding.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // List to store the combinations
+        List<List<Integer>> combinations = new ArrayList<>();
+        // Special case
+        if (candidates == null || candidates.length == 0) {
+            return combinations;
+        }
+        // Sort the array
+        Arrays.sort(candidates);
+        // Perform backtracking operation
+        backtrack(candidates, 0, new ArrayList<>(), combinations, target);
+        return combinations;
+    }
+
+    private void backtrack(int[] candidates, int index, List<Integer> combination, List<List<Integer>> combinations, int target) {
+        // Base cases
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            combinations.add(new ArrayList<>(combination));
+        }
+        // Traverse for all the candidates
+        for (int i = index; i < candidates.length; i++) {
+            // Add current element to the list
+            combination.add(candidates[i]);
+            // Backtrack
+            backtrack(candidates, i, combination, combinations, target - candidates[i]);
+            // Discard current element
+            combination.remove(combination.size() - 1);
+        }
+    }
+}
---
+++ src/test/java/com/anirudhology/leetcoding/array/CombinationSumTest.java
@@ -0,0 +1,44 @@
+package com.anirudhology.leetcoding.array;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CombinationSumTest {
+
+    private final CombinationSum combinationSum = new CombinationSum();
+
+    @Test
+    public void testCombinationSumWithEmptyCandidatesShouldReturnEmptyList() {
+        int[] candidates = {};
+        int target = 7;
+        List<List<Integer>> expected = new ArrayList<>();
+        List<List<Integer>> actual = combinationSum.combinationSum(candidates, target);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCombinationSumWithTargetZeroShouldReturnEmptyList() {
+        int[] candidates = {2, 3, 6, 7};
+        int target = 0;
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Collections.emptyList());
+        List<List<Integer>> actual = combinationSum.combinationSum(candidates, target);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCombinationSumWithValidCandidatesAndTargetShouldReturnCorrectCombinations() {
+        int[] candidates = {2, 3, 6, 7};
+        int target = 7;
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(List.of(2, 2, 3));
+        expected.add(List.of(7));
+        List<List<Integer>> actual = combinationSum.combinationSum(candidates, target);
+        assertEquals(expected, actual);
+    }
+}
