5baa9031d97b4e99010bd292879dd1a1b9b37cd0
Anirudh Sharma <anirudhology@gmail.com>
2024-04-26 05:02:19 +0530

    LeetCode #1020 - Number Of Enclaves
    Add problem solution

---
+++ src/main/java/com/anirudhology/leetcoding/NumberOfEnclaves.java
@@ -0,0 +1,49 @@
+package com.anirudhology.leetcoding;
+
+public class NumberOfEnclaves {
+
+    public int numEnclaves(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        // Dimensions of the grid
+        int m = grid.length;
+        int n = grid[0].length;
+        // Traverse through the grid
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == m - 1 || j == n - 1) {
+                    dfs(grid, i, j, m, n);
+                }
+            }
+        }
+        // Count of enclaves
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(int[][] grid, int i, int j, int m, int n) {
+        // Base case
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return;
+        }
+        // If current cell is not land, we will return
+        if (grid[i][j] != 1) {
+            return;
+        }
+        // Mark current cell as visited
+        grid[i][j] = -1;
+        // Perform DFS on neighbors recursively
+        dfs(grid, i + 1, j, m, n);
+        dfs(grid, i - 1, j, m, n);
+        dfs(grid, i, j + 1, m, n);
+        dfs(grid, i, j - 1, m, n);
+    }
+}
---
+++ src/test/java/com/anirudhology/leetcoding/NumberOfEnclavesTest.java
@@ -0,0 +1,45 @@
+package com.anirudhology.leetcoding;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NumberOfEnclavesTest {
+
+    private final NumberOfEnclaves numberOfEnclaves = new NumberOfEnclaves();
+
+    @Test
+    public void testNumEnclavesBasic() {
+        int[][] grid = {
+                {0, 0, 0, 0},
+                {1, 0, 1, 0},
+                {0, 1, 1, 0},
+                {0, 0, 0, 0}
+        };
+        assertEquals(3, numberOfEnclaves.numEnclaves(grid));
+    }
+
+    @Test
+    public void testNumEnclavesEmptyGrid() {
+        int[][] grid = {};
+        assertEquals(0, numberOfEnclaves.numEnclaves(grid));
+    }
+
+    @Test
+    public void testNumEnclavesAllEnclaves() {
+        int[][] grid = {
+                {1, 1},
+                {1, 1}
+        };
+        assertEquals(0, numberOfEnclaves.numEnclaves(grid));
+    }
+
+    @Test
+    public void testNumEnclavesNoEnclaves() {
+        int[][] grid = {
+                {0, 0},
+                {0, 0}
+        };
+        assertEquals(0, numberOfEnclaves.numEnclaves(grid));
+    }
+}
