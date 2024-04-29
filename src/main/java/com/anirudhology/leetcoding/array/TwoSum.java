03150df440befa73173cfc8be9b225b84afe1846
Anirudh Sharma <anirudhology@gmail.com>
2024-04-29 07:21:38 +0530

    LeetCode #1 - Two Sum
    Fixed typo in variable name

--- src/main/java/com/anirudhology/leetcoding/array/TwoSum.java
+++ src/main/java/com/anirudhology/leetcoding/array/TwoSum.java
@@ -12,18 +12,18 @@ public class TwoSum {
         }
         // Map to store the difference of target and current element as key and
         // index of current element as value;
-        Map<Integer, Integer> compliments = new HashMap<>();
+        Map<Integer, Integer> complements = new HashMap<>();
         // Traverse through the array
         for (int i = 0; i < nums.length; i++) {
             // Compliment of current element w.r.t target
             int compliment = target - nums[i];
             // Check if we have already found the difference
-            if (compliments.containsKey(compliment)) {
-                return new int[]{compliments.get(compliment), i};
+            if (complements.containsKey(compliment)) {
+                return new int[]{complements.get(compliment), i};
             }
-            compliments.put(nums[i], i);
+            complements.put(nums[i], i);
         }
-        System.out.println(compliments);
+        System.out.println(complements);
         // Should never reach here
         throw new IllegalArgumentException("Invalid input(s).");
     }
