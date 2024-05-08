2e9b690f3dbbb0f9ef7f6c2bb696e5714abb7cff
Anirudh Sharma <anirudhology@gmail.com>
2024-05-09 05:11:27 +0530

    LeetCode #920 - Meeting Rooms
    Refactor code with correct variable names

--- src/main/java/com/anirudhology/leetcoding/array/MeetingRooms.java
+++ src/main/java/com/anirudhology/leetcoding/array/MeetingRooms.java
@@ -5,13 +5,17 @@ import java.util.Comparator;
 
 public class MeetingRooms {
 
+    /**
+     * Time Complexity - O(N*log(N))
+     * Space Complexity - O(1)
+     */
     public boolean canAttendMeetings(int[][] intervals) {
         // Special case
         if (intervals == null || intervals.length == 0) {
             throw new IllegalArgumentException("There are no meetings to attend!");
         }
         // Sort the array by start time of meetings
-        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
         // Traverse through the meetings one by one
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i - 1][1] > intervals[i][0]) {
