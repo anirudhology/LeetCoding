07fe5fb1a4473b64e3eefea0ca4e25b98ef9d279
Anirudh Sharma <anirudhology@gmail.com>
2024-05-08 18:56:04 +0530

    LeetCode #1 - Two Sum
    Update code with brute force and suboptimal implementations

--- src/main/java/com/anirudhology/leetcoding/array/TwoSum.java
+++ src/main/java/com/anirudhology/leetcoding/array/TwoSum.java
@@ -5,7 +5,75 @@ import java.util.Map;
 
 public class TwoSum {
 
-    public int[] twoSum(int[] nums, int target) {
+    /**
+     * Time Complexity - O(N^2)
+     * Space Complexity - O(1)
+     */
+    public int[] twoSumBruteForce(int[] nums, int target) {
+        // Special case
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Array cannot be null or empty!");
+        }
+        // Length of the array
+        int n = nums.length;
+        // Output array
+        int[] result = new int[2];
+        // Check for all combinations
+        for (int i = 0; i < n; i++) {
+            // Check for all other numbers
+            for (int j = i + 1; j < n; j++) {
+                // Solution is found!!!
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Time Complexity - O(N)
+     * Space Complexity - O(N)
+     */
+    public int[] twoSumSuboptimal(int[] nums, int target) {
+        // Special case
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Array cannot be null or empty!");
+        }
+        // Length of the array
+        int n = nums.length;
+        // Map to store the elements and its index
+        Map<Integer, Integer> elements = new HashMap<>();
+        // Output array
+        int[] result = new int[2];
+        // Populate the map
+        for (int i = 0; i < n; i++) {
+            elements.put(nums[i], i);
+        }
+        // Traverse through the array again to check for combination
+        // that gives us target value
+        for (int i = 0; i < n; i++) {
+            // Calculate the complement
+            int complement = target - nums[i];
+            // Check if complement exists in the array/map, and it is
+            // not the same element
+            if (elements.containsKey(complement) && elements.get(complement) != i) {
+                // We have found the solution
+                result[0] = i;
+                result[1] = elements.get(complement);
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Time Complexity - O(N)
+     * Space Complexity - O(N)
+     */
+    public int[] twoSumOptimal(int[] nums, int target) {
         // Check if array is not null or empty
         if (nums == null || nums.length == 0) {
             throw new IllegalArgumentException("Array must have at least two elements.");
@@ -13,18 +81,20 @@ public class TwoSum {
         // Map to store the difference of target and current element as key and
         // index of current element as value;
         Map<Integer, Integer> complements = new HashMap<>();
+        // Output array
+        int[] result = new int[2];
         // Traverse through the array
         for (int i = 0; i < nums.length; i++) {
             // Compliment of current element w.r.t target
             int compliment = target - nums[i];
             // Check if we have already found the difference
             if (complements.containsKey(compliment)) {
-                return new int[]{complements.get(compliment), i};
+                result[0] = i;
+                result[1] = complements.get(compliment);
+                break;
             }
             complements.put(nums[i], i);
         }
-        System.out.println(complements);
-        // Should never reach here
-        throw new IllegalArgumentException("Invalid input(s).");
+        return result;
     }
 }
--- src/test/java/com/anirudhology/leetcoding/array/TwoSumTest.java
+++ src/test/java/com/anirudhology/leetcoding/array/TwoSumTest.java
@@ -2,36 +2,55 @@ package com.anirudhology.leetcoding.array;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TwoSumTest {
 
     private final TwoSum twoSum = new TwoSum();
 
     @Test
-    void testTwoSum() {
-        // Test case 1: Valid input
-        int[] nums1 = {2, 7, 11, 15};
-        int target1 = 9;
-        int[] expected1 = {0, 1};
-        assertArrayEquals(expected1, twoSum.twoSum(nums1, target1));
+    public void testTwoSumBruteForce() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] expected = {0, 1};
+        assertArrayEquals(expected, twoSum.twoSumBruteForce(nums, target));
+    }
 
-        // Test case 2: Empty array
-        int[] nums2 = {};
-        int target2 = 9;
-        IllegalArgumentException exception2 = assertThrows(IllegalArgumentException.class, () -> twoSum.twoSum(nums2, target2));
-        assertEquals("Array must have at least two elements.", exception2.getMessage());
+    @Test
+    public void testTwoSumSuboptimal() {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] expected = {1, 2};
+        assertArrayEquals(expected, twoSum.twoSumSuboptimal(nums, target));
+    }
 
-        // Test case 3: Null array
-        int[] nums3 = null;
-        int target3 = 9;
-        IllegalArgumentException exception3 = assertThrows(IllegalArgumentException.class, () -> twoSum.twoSum(nums3, target3));
-        assertEquals("Array must have at least two elements.", exception3.getMessage());
+    @Test
+    public void testTwoSumOptimal() {
+        int[] nums = {3, 3};
+        int target = 6;
+        int[] expected = {1, 0};
+        assertArrayEquals(expected, twoSum.twoSumOptimal(nums, target));
+    }
 
-        // Test case 4: No solution
-        int[] nums4 = {1, 2, 3, 4};
-        int target4 = 9;
-        IllegalArgumentException exception4 = assertThrows(IllegalArgumentException.class, () -> twoSum.twoSum(nums4, target4));
-        assertEquals("Invalid input(s).", exception4.getMessage());
+    @Test
+    public void testTwoSumBruteForce_Exception() {
+        int[] nums = {};
+        int target = 9;
+        assertThrows(IllegalArgumentException.class, () -> twoSum.twoSumBruteForce(nums, target));
+    }
+
+    @Test
+    public void testTwoSumSuboptimal_Exception() {
+        int[] nums = {};
+        int target = 9;
+        assertThrows(IllegalArgumentException.class, () -> twoSum.twoSumSuboptimal(nums, target));
+    }
+
+    @Test
+    public void testTwoSumOptimal_Exception() {
+        int[] nums = {};
+        int target = 9;
+        assertThrows(IllegalArgumentException.class, () -> twoSum.twoSumOptimal(nums, target));
     }
 }
