fde922b7e901c2badd666d2686b1c94f2138d0ae
Anirudh Sharma <anirudhology@gmail.com>
2024-05-09 06:08:34 +0530

    LeetCode #977 - Squares Of Sorted Array
    Update brute force solution

--- src/main/java/com/anirudhology/leetcoding/array/SquaresOfSortedArray.java
+++ src/main/java/com/anirudhology/leetcoding/array/SquaresOfSortedArray.java
@@ -1,25 +1,55 @@
 package com.anirudhology.leetcoding.array;
 
+import java.util.Arrays;
+
 public class SquaresOfSortedArray {
 
-    public int[] sortedSquares(int[] nums) {
+    /**
+     * Time Complexity - O(N*log(N))
+     * Space Complexity - O(1)
+     */
+    public int[] sortedSquaresBruteForce(int[] nums) {
         // Special case
         if (nums == null || nums.length == 0) {
-            throw new IllegalArgumentException("Array cannot be null or empty.");
+            return nums;
         }
         // Length of the array
         int n = nums.length;
-        // Array to store squares
+        // Array to store squares of elements
+        int[] squares = new int[n];
+        // Calculate the squares of elements
+        for (int i = 0; i < n; i++) {
+            squares[i] = nums[i] * nums[i];
+        }
+        // Sort the squares array
+        Arrays.sort(squares);
+        return squares;
+    }
+
+    /**
+     * Time Complexity - O(N)
+     * Space Complexity - O(1)
+     */
+    public int[] sortedSquaresOptimal(int[] nums) {
+        // Special case
+        if (nums == null || nums.length == 0) {
+            return nums;
+        }
+        // Length of the array
+        int n = nums.length;
+        // Array to store squares of elements
         int[] squares = new int[n];
         // Left and right pointers
         int left = 0;
         int right = n - 1;
-        // Index to keep track of current element
+        // Index to keep track of bigger square
         int index = n - 1;
-        // Traverse through the array from both sides
-        while (left <= right) {
+        // Process the array
+        for (int i = 0; i < n; i++) {
+            // Calculate squares at both left and right indices
             int leftSquare = nums[left] * nums[left];
             int rightSquare = nums[right] * nums[right];
+            // Place the squares at correct place
             if (leftSquare > rightSquare) {
                 squares[index] = leftSquare;
                 left++;
--- src/test/java/com/anirudhology/leetcoding/array/SquaresOfSortedArrayTest.java
+++ src/test/java/com/anirudhology/leetcoding/array/SquaresOfSortedArrayTest.java
@@ -13,30 +13,33 @@ class SquaresOfSortedArrayTest {
         // Normal case: sorted array with positive and negative numbers
         int[] nums1 = {-4, -1, 0, 3, 10};
         int[] expected1 = {0, 1, 9, 16, 100};
-        assertArrayEquals(expected1, squaresOfSortedArray.sortedSquares(nums1));
+        assertArrayEquals(expected1, squaresOfSortedArray.sortedSquaresBruteForce(nums1));
+        assertArrayEquals(expected1, squaresOfSortedArray.sortedSquaresOptimal(nums1));
 
         // Normal case: sorted array with all negative numbers
         int[] nums2 = {-7, -3, -2, -1};
         int[] expected2 = {1, 4, 9, 49};
-        assertArrayEquals(expected2, squaresOfSortedArray.sortedSquares(nums2));
+        assertArrayEquals(expected2, squaresOfSortedArray.sortedSquaresBruteForce(nums2));
+        assertArrayEquals(expected2, squaresOfSortedArray.sortedSquaresOptimal(nums2));
 
         // Normal case: sorted array with all positive numbers
         int[] nums3 = {1, 2, 3, 4, 5};
         int[] expected3 = {1, 4, 9, 16, 25};
-        assertArrayEquals(expected3, squaresOfSortedArray.sortedSquares(nums3));
+        assertArrayEquals(expected3, squaresOfSortedArray.sortedSquaresBruteForce(nums3));
+        assertArrayEquals(expected3, squaresOfSortedArray.sortedSquaresOptimal(nums3));
     }
 
     @Test
     void testSortedSquaresSpecialCase() {
         // Special case: empty array
         int[] nums4 = {};
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> squaresOfSortedArray.sortedSquares(nums4));
-        assertEquals("Array cannot be null or empty.", exception.getMessage());
+        assertArrayEquals(nums4, squaresOfSortedArray.sortedSquaresBruteForce(nums4));
+        assertArrayEquals(nums4, squaresOfSortedArray.sortedSquaresOptimal(nums4));
 
         // Special case: null array
         int[] nums5 = null;
-        exception = assertThrows(IllegalArgumentException.class, () -> squaresOfSortedArray.sortedSquares(nums5));
-        assertEquals("Array cannot be null or empty.", exception.getMessage());
+        assertArrayEquals(nums5, squaresOfSortedArray.sortedSquaresBruteForce(nums5));
+        assertArrayEquals(nums5, squaresOfSortedArray.sortedSquaresOptimal(nums5));
     }
 
     @Test
@@ -44,16 +47,19 @@ class SquaresOfSortedArrayTest {
         // Single element in the array
         int[] nums6 = {5};
         int[] expected6 = {25};
-        assertArrayEquals(expected6, squaresOfSortedArray.sortedSquares(nums6));
+        assertArrayEquals(expected6, squaresOfSortedArray.sortedSquaresBruteForce(nums6));
+        assertArrayEquals(expected6, squaresOfSortedArray.sortedSquaresOptimal(nums6));
 
         // Single negative element in the array
         int[] nums7 = {-3};
         int[] expected7 = {9};
-        assertArrayEquals(expected7, squaresOfSortedArray.sortedSquares(nums7));
+        assertArrayEquals(expected7, squaresOfSortedArray.sortedSquaresBruteForce(nums7));
+        assertArrayEquals(expected7, squaresOfSortedArray.sortedSquaresOptimal(nums7));
 
         // Single zero element in the array
         int[] nums8 = {0};
         int[] expected8 = {0};
-        assertArrayEquals(expected8, squaresOfSortedArray.sortedSquares(nums8));
+        assertArrayEquals(expected8, squaresOfSortedArray.sortedSquaresBruteForce(nums8));
+        assertArrayEquals(expected8, squaresOfSortedArray.sortedSquaresOptimal(nums8));
     }
 }
